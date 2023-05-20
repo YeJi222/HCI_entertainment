500a45a551e44d2ff1a40c1d8ec1809f5615d987
YeJi222 <70511859+YeJi222@users.noreply.github.com>
2023-05-20 15:56:07 +0900

    feat: success to multiThread function

--- multiThread.py
+++ multiThread.py
@@ -27,14 +27,12 @@ def putFps(img): # fps 표시
 	fps_txt = "%01.f" % fps_val
 	cv2.putText(img, fps_txt, (0, 25), cv2.FONT_HERSHEY_DUPLEX, 1, (0, 255, 0))
 
-def camera_first(cam_id, capture, q):
+def camera_first(cam_id, capture, q, accelQ, accelFlag):
     # print("camera 1")
     # print("cam_id : " + str(cam_id))
     
     # capture = cv2.VideoCapture(cam_id)
     frame_captured, frame = capture.read();
-    
-    # print(frame_captured)
     
     if frame_captured:
         markers = detect_markers(frame)
@@ -50,18 +48,14 @@ def camera_first(cam_id, capture, q):
             # print("camera1 coordinate", centerX, centerY)
             
             if 650 < centerX < 1245 and 30 < centerY < 780: # accel
-                # keyboard.press('a')
-                # print(centerX, centerY)
-                eccelFlag = 1
-                print("Eccel")
+                accelFlag = 1
+                # print("accel in camera_first", accelFlag)
             else:
-                eccelFlag = 0
-                # print()
-                # keyboard.release('a')
-
-			# print(centerX, centerY);
-			# print("detect!")
+                accelFlag = 0
+                # print("accel in camera_first", accelFlag)
 		
+        # print("accel Flag in camera_first", accelFlag)
+        accelQ.put(accelFlag)
 		
         frame_captured, frame = capture.read()
         frame = np.flip(frame, axis=1)
@@ -72,15 +66,14 @@ def camera_first(cam_id, capture, q):
         width = frame.shape[1]
 	
         frame = cv2.rectangle(frame,(width//3,0),(width//3 + 300 ,height//3+ 200),(255,255,255),3)
-		# cv2.putText(frame,'LEFT',(160,30),cv2.FONT_HERSHEY_DUPLEX,1,(255,255,255), 2)
+        cv2.putText(frame,'Accel',(400,30),cv2.FONT_HERSHEY_DUPLEX,1,(255,255,255), 2)
 
-        # cv2.imshow('Camera1', frame)
         q.put(frame)
     
     return frame;
     
     
-def camera_second(cam_id, capture, q2):
+def camera_second(cam_id, capture, q2, accelFlag):
     # print("camera 2")
     # print("cam_id : " + str(cam_id))
     frame_captured, frame = capture.read();
@@ -90,30 +83,30 @@ def camera_second(cam_id, capture, q2):
     if frame_captured:
         markers = detect_markers(frame)
         centerX = 0
-        centerY = 0
+        # centerY = 0
         
         # print(centerX, centerY)
         
         for marker in markers:
             marker.highlite_marker(frame)
             centerX = marker.center[0]
-            centerY = marker.center[1]
-            
+            # centerY = marker.center[1]
             # print("camera2 coordinate", centerX, centerY)
-            print("eccel", eccelFlag)
             
-            if centerX < 400 and eccelFlag == 1:
+            if centerX > 1025 and accelFlag == 1: # front
+                keyboard.press('w')
                 print("Front")
-                front = 1
-                back = 0
-            if centerX > 450 and eccelFlag == 1:
+            else:
+                keyboard.release('w')
+            if centerX < 888 and accelFlag == 1: # back
+                keyboard.release('w')
+                keyboard.press('s')
                 print("Back")
-                front = 0
-                back = 0
+            else:
+                keyboard.release('s')
 
 			# print(centerX, centerY);
 			# print("detect!")
-		
 		
         frame_captured, frame = capture.read()
         frame = np.flip(frame, axis=1)
@@ -130,8 +123,6 @@ def camera_second(cam_id, capture, q2):
         cv2.putText(frame,'BACK',(width - width//4- 20,30),cv2.FONT_HERSHEY_DUPLEX,1,(255,255,255), 2)
 		
         q2.put(frame)
-        
-    # print("Second Camera")
 
 if __name__ == '__main__':
 	cam_id = 0 #default 0
@@ -141,49 +132,43 @@ if __name__ == '__main__':
 	print('cam_id : ', argv[2])
 	cam_id = int(argv[2])
 	cam_id2 = cam_id + 1;
-	cam_id3 = cam_id + 2;
  
 	capture = cv2.VideoCapture(cam_id)
 	capture2 = cv2.VideoCapture(cam_id2)
-	# capture3 = cv2.VideoCapture(cam_id3)
  
 	frame_captured, frame = capture.read();
  
-	eccelFlag = 0
-	front = 0
-	back = 0
+	accelFlag = 0
 	
 	q = queue.Queue()
 	q2 = queue.Queue()
- 
+	accelQ = queue.Queue()
+	accelQ.put(0)
+    
 	while frame_captured:
-		thread_1 = threading.Thread(target = camera_first, args=(cam_id, capture, q))
-		thread_2 = threading.Thread(target = camera_second, args=(cam_id, capture, q2))
-		# thread_3 = threading.Thread(target = camera_third)
+		accelFlag = accelQ.get()
+		# print("accel", accelFlag)
+        
+		thread_1 = threading.Thread(target = camera_first, args=(cam_id, capture, q, accelQ, accelFlag))
+		thread_2 = threading.Thread(target = camera_second, args=(cam_id, capture, q2, accelFlag))
 	
 		thread_1.start()
 		thread_2.start()
-		# thread_3.start()
-		
+  
+		# print("accel", accelFlag)
   
 		frame = q.get()
 		frame2 = q2.get()
-		# print(type(frame))
 		if cv2.waitKey(1) & 0xFF == ord('q'):
 			break
 
 		cv2.imshow('Camera1', frame)
 		cv2.imshow('Camera2', frame2)
   
-		# 'q' 키를 누르면 종료
-		if cv2.waitKey(1) & 0xFF == ord('q'):
-			break
-  
 		thread_1.join()
 		thread_2.join()
-		# thread_3.join()
 		
 	# When everything done, release the capture
 	capture.release()
-	# capture2.release()
+	capture2.release()
 	cv2.destroyAllWindows()
